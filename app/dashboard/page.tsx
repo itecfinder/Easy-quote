93540890ec257575bb781d8dab81b891521e39e9
itecfinder <businessadviserpro@gmail.com>
2026-06-29 17:58:54 -0400

    Delete app directory

--- app/dashboard/page.tsx
+++
@@ -1,9 +0,0 @@
-import { redirect } from 'next/navigation';
-import { getSession } from '@/lib/session';
-import { AppShell } from '@/components/app/app-shell';
-
-export default async function DashboardPage() {
-  const session = await getSession();
-  if (!session) redirect('/login');
-  return <AppShell session={session} />;
-}
