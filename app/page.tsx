772e37a1a84c55eb56f0455059f7af956bcd5031
itecfinder <businessadviserpro@gmail.com>
2026-07-10 17:17:42 -0400

    Replace DashboardScreen with ScreenRouter in HomePage

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,19 @@
 "use client";
 
-import AppShell from "@/components/layout/app-shell";
-import DashboardScreen from "@/components/screens/dashboard-screen";
+import { AppShell } from "@/components/layout/app-shell";
+import ScreenRouter from "@/components/screens/screen-router";
 
-export default function HomePage() {
-  return (
-    <AppShell>
-      <DashboardScreen />
-    </AppShell>
-  );
+
+export default function HomePage(){
+
+return (
+
+<AppShell>
+
+<ScreenRouter/>
+
+</AppShell>
+
+);
+
 }
