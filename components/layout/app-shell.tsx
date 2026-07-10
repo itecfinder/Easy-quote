15ae18546b23af7bfc9fc2408db9d172b47f2256
itecfinder <businessadviserpro@gmail.com>
2026-07-10 17:14:29 -0400

    Replace AppProvider with ScreenProvider in AppShell

--- components/layout/app-shell.tsx
+++ components/layout/app-shell.tsx
@@ -1,27 +1,35 @@
-'use client';
+"use client";
 
-import { AppProvider } from '@/context/app-context';
-import { Sidebar } from './sidebar';
-import { Topbar } from './topbar';
+import { ScreenProvider } from "@/context/screen-context";
+import { Sidebar } from "./sidebar";
+import { Topbar } from "./topbar";
+
 
 export function AppShell({
   children,
 }: {
   children: React.ReactNode;
 }) {
+
   return (
-    <AppProvider>
-      <div className="min-h-screen bg-background flex">
+    <ScreenProvider>
+
+      <div className="min-h-screen flex bg-background">
+
         <Sidebar />
 
-        <div className="flex-1 flex flex-col min-w-0">
+        <div className="flex-1 flex flex-col">
+
           <Topbar />
 
           <main className="flex-1 p-6">
             {children}
           </main>
+
         </div>
+
       </div>
-    </AppProvider>
+
+    </ScreenProvider>
   );
 }
