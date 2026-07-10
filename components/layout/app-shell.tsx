855da1fac49316400030c978f812ff491cfed295
itecfinder <businessadviserpro@gmail.com>
2026-07-10 16:57:37 -0400

    Fix import statement for AppShell component

--- components/layout/app-shell.tsx
+++ components/layout/app-shell.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { AppShell } from "@/components/layout/app-shell";
+import AppShell from "@/components/layout/app-shell";
 import DashboardScreen from "@/components/screens/dashboard-screen";
 
 export default function HomePage() {
