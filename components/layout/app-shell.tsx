8f4947c78ed1572182b4f3e3e568dc2231a9b284
itecfinder <businessadviserpro@gmail.com>
2026-07-10 16:53:02 -0400

    Refactor AppShell to HomePage with DashboardScreen
    Refactor AppShell component into HomePage and integrate DashboardScreen.

--- components/layout/app-shell.tsx
+++ components/layout/app-shell.tsx
@@ -1,29 +1,12 @@
-'use client';
+"use client";
 
-import { AppProvider } from '@/context/app-context';
-import { Sidebar } from './sidebar';
-import { Topbar } from './topbar';
+import { AppShell } from "@/components/layout/app-shell";
+import DashboardScreen from "@/components/screens/dashboard-screen";
 
-export function AppShell({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
+export default function HomePage() {
   return (
-    <AppProvider>
-      <div className="min-h-screen bg-background flex">
-        <Sidebar />
-
-        <div className="flex-1 flex flex-col min-w-0">
-          <Topbar />
-
-          <main className="flex-1 p-4 lg:p-8 pt-20 lg:pt-8 overflow-x-hidden">
-            <div className="max-w-5xl mx-auto">
-              {children}
-            </div>
-          </main>
-        </div>
-      </div>
-    </AppProvider>
+    <AppShell>
+      <DashboardScreen />
+    </AppShell>
   );
 }
